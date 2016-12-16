68dcbd44d14859f3c5ba86f6da27e97dcf620641
Zsolt Hajdu <z_hajdu@yahoo.com>
2016-12-15 23:17:12 -0800

    Added solution of MinAvgTwoSlice puzzle.

---
+++ PrefixSums/MinAvgSlice.cpp
@@ -0,0 +1,39 @@
+
+// https://codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
+// 
+
+#include <algorithm>
+#include <iostream>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+
+	unsigned 	int i = 0;
+
+	if( A.size() < 2 )
+		return 0;
+
+	double min = (double)(A[0] + A[1] ) /2;
+	int minPos = 0;
+
+	for( i=1 ; i < (A.size()-1) ; i++ ) {
+		double n = (double)(A[i] + A[i+1]) / 2;
+		if( n < min ) {
+			min = n;
+			minPos = i;
+		}
+	}
+	
+	for( i=0 ; i<(A.size()-2) ; i++ ) {
+		double n = (double)(A[i] + A[i+1] + A[i+2]) / 3;
+		if( n < min ) {
+			min = n;
+			minPos = i;
+		}
+	}
+
+	return minPos;
+}
+
