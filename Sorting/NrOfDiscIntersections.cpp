e08c0a0eeeca30f8af63435596420ddf401e02f0
Zsolt Hajdu <z_hajdu@yahoo.com>
2017-07-23 09:48:21 -0700

    Improved solution for 'Disc Intersections' challenge.

--- Sorting/NrOfDiscIntersections.cpp
+++ Sorting/NrOfDiscIntersections.cpp
@@ -1,7 +1,7 @@
 
 // Disc intersetion challenge
 // https://codility.com/programmers/lessons/6-sorting/number_of_disc_intersections/
-//
+// 100% solution
 
 #include <algorithm>
 #include <vector>
@@ -12,57 +12,55 @@ using namespace std;
 const int MYMAX = 10000000;
 
 int solution(vector<int> &A) {
-	auto cnt = 0;    
+    auto cntIntersections = 0;
 
-	vector<int> left;
-	vector<int>  right;
+    vector<long long> left;
+    vector<long long> right;
 	for( unsigned int i=0 ; i<A.size() ; i++ ) {
-		left.push_back( i - A[i] );
-		right.push_back( i + A[i] );
+        left.push_back( (long long)i - A[i] );
+        right.push_back( (long long)i + A[i] );
 	}
 	sort( left.begin(), left.end() );
 	sort( right.begin(), right.end() );
 
-	// Current left/right position values, as we step through the
-	// sorted positions and sum current overlaps
-	int cLeft = 0, cRight = 0;
-	// Indices into the sorted left/right end pos arrays
-	auto iRight = 0, iLeft=0;
-	auto leftIndexAdjust = 1;
+    auto discCount = 1;
+    size_t lPos = 0, rPos = 0;
+    for( lPos = 1 ; lPos<left.size() && rPos<right.size() ; ) {
+        if( left[lPos] <= right[rPos] ) {
+            // New disc starting
+            cntIntersections += discCount;
+            discCount++;
+            lPos++;
 
-	for( iRight=0 ; iRight < right.size() ; ++iRight ) {
-		cRight = right[iRight];
-		
-		for( ; iLeft < left.size() && cLeft <= cRight ; ++iLeft ) {
-			cLeft = left[ iLeft ];
-		}
-		if( iLeft == left.size() && cLeft <= cRight ) {
-			// Done with left edges, 
-			leftIndexAdjust = 0;
-		}
+            if( cntIntersections > MYMAX )
+                return -1;
+        }
+        else if( right[rPos] < left[lPos]) {
+            // A disk ended
+            discCount--;
+            rPos++;
+        }
+        else if( right[rPos] == left[lPos] ) {
+            // One ended, another one starting
+            lPos++;
+            rPos++;
+        }
 
-		// -1 is to account for left edge of cRight which is not
-		// an overlap
-		cnt += ( iLeft - 1 - leftIndexAdjust - iRight );
-		
-		if( cnt > MYMAX )
-			return -1;
-	}
-	return cnt;
+    }
+    return cntIntersections;
 }
 
 int main( int argc, char ** argv )
 {
-	vector<int> A;
-
-	A.push_back( 1 );
-	A.push_back( 5 );
-	A.push_back( 2 );
-	A.push_back( 1 );
-	A.push_back( 4 );
-	A.push_back( 0 );
+    vector<int> A{ 1,5,2,1,4,0 };
 
 	cout << "Intersections : " << solution( A ) << endl;
+    cout << "Correct answer is : 11" << endl;
+
+    vector<int> B{ 1, 2147483647, 0 };
+
+    cout << "Intersections : " << solution( B ) << endl;
+    cout << "Correct answer is : 2" << endl;
 
 	return 0;
 }
